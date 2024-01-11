6d89334276cea5039b4d47b8cc50c597c8fe06bd
Elton Harrison <eltonharrison14@gmail.com>
2024-01-11 11:45:28 -0700

    complete and test ChessPosition

---
+++ shared/src/main/java/chess/Testing.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class Testing {
+    public static void main(String[] args) {
+        ChessPosition newPos = new ChessPosition(1,2);
+        System.out.println(newPos.getRow());
+        System.out.println(newPos.getColumn());
+
+    }
+}

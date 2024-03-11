412c5a100e3a36ec34f691fd41ee0ddcb65dd199
Elton Harrison <eltonharrison14@gmail.com>
2024-03-11 15:21:47 -0600

    changes server string

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,8 +3,9 @@ import server.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("â™• 240 Chess Client: " + piece);
+            var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            System.out.println("♕ 240 Chess Client: " + piece);
+
 
         Server server= new Server();
         server.run(8080);

35fab92e3df14461948ed71a68e6702dd7007844
Elton Harrison <eltonharrison14@gmail.com>
2024-03-19 20:52:38 -0600

    begant to fill out communicator class

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,16 +1,79 @@
 import chess.*;
 import ui.PrintBoard;
+import java.util.Scanner;
 
 public class Client
 {
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         ChessGame game = new ChessGame();
+    }
+
+    private void listLoginOption(){
+        System.out.println("Please select an option by entering the number");
+        System.out.println("1.Register");
+        System.out.println("2.Login");
+        System.out.println("3.Help");
+        System.out.println("4.Quit");
+    }
+    public void getLoginOption(){
+
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        switch(a){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+    }
+    private void listUserOption(){
+        System.out.println("Please select an option by entering the number");
+        System.out.println("1.Logout");
+        System.out.println("2.List Games");
+        System.out.println("3.Create Game");
+        System.out.println("4.Join Game");
+        System.out.println("5.Join Observer");
+        System.out.println("6.Help");
+    }
+    public void getUserOption(){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        switch(a){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+    private void printBlack(ChessGame game){
+        PrintBoard printing = new PrintBoard(game);
+        printing.printBoardBlack();
+    }
+
+    private void printWhite(ChessGame game){
         PrintBoard printing = new PrintBoard(game);
         printing.printBoardWhite();
-        System.out.println("\n\n");
-        printing.printBoardBlack();
-
     }
 }
--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,3 +1,24 @@
 public class ServerFacade
 {
+    public void register(){
+
+    }
+    public void login(){
+
+    }
+    public void logout(){
+
+    }
+    public void listGames(){
+
+    }
+    public void joinGame(){
+
+    }
+    public void observeGame(){
+
+    }
+    public void createGame(){
+
+    }
 }

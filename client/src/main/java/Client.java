eee7d874e0f688bf74c255c5d781080d9781c57c
Elton Harrison <eltonharrison14@gmail.com>
2024-03-21 17:29:50 -0600

    finished rough draft for all code

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,42 +1,89 @@
 import chess.*;
+import model.User;
+import records.CreateGameRequest;
+import records.GameReturn;
+import records.JoinGameRequest;
+import records.LoginRequest;
 import ui.PrintBoard;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client
 {
     private String token;
+    private ServerFacade server;
 
-    public static void main(String[] args) {
+    ArrayList<GameReturn> games;
+
+    public static void main(String[] args)
+    {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         ChessGame game = new ChessGame();
+
     }
 
-    private void listLoginOption(){
+    private void listLoginOption()
+    {
         System.out.println("Please select an option by entering the number");
         System.out.println("1.Register");
         System.out.println("2.Login");
-        System.out.println("3.Help");
-        System.out.println("4.Quit");
+        System.out.println("3.Quit");
+        System.out.println("4.Help");
     }
-    public void getLoginOption(){
 
+    public int getLoginOption(ServerFacade sf)
+    {
         Scanner in = new Scanner(System.in);
+        String username;
+        String password;
         int a = in.nextInt();
-        switch(a){
-            case 1:
+        switch (a)
+        {
+            case 1: //register
+                username = in.nextLine();
+                password = in.nextLine();
+                String email = in.nextLine();
+                try
+                {
+                    token = sf.register(new User(username, password, email)).authToken();
+                    System.out.println("You successfully Registered");
+                } catch (IOException ex)
+                {
+                    System.out.println("You failed to Register");
+                }
                 break;
-            case 2:
+            case 2:// Login
+                username = in.nextLine();
+                password = in.nextLine();
+                try
+                {
+                    token = sf.login(new LoginRequest(username, password)).authToken();
+                    System.out.println("You successfully Logged in");
+                } catch (IOException ex)
+                {
+                    System.out.println("You failed to Login");
+                }
                 break;
-            case 3:
+            case 3: // quit
+                System.out.println("You are closing the program");
                 break;
-            case 4:
+            case 4: // Help
+                System.out.println("Press 1 to register as a new player \n" +
+                        "Press 2 to login\n" +
+                        "Press 3 to Quit the program");
                 break;
+
             default:
                 break;
         }
+        return a;
     }
-    private void listUserOption(){
+
+    private void listUserOption()
+    {
         System.out.println("Please select an option by entering the number");
         System.out.println("1.Logout");
         System.out.println("2.List Games");
@@ -45,36 +92,163 @@ public class Client
         System.out.println("5.Join Observer");
         System.out.println("6.Help");
     }
-    public void getUserOption(){
+
+    public void getUserOption(ServerFacade sf)
+    {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        switch(a){
+        switch (a)
+        {
             case 1://Logout
+                logout(sf);
                 break;
-            case 2:
+            case 2:// List Games
+                listGames(sf);
                 break;
-            case 3:
+            case 3://create game
+                createGame(sf);
                 break;
-            case 4:
+            case 4://join game
+                joinGame(sf);
                 break;
-            case 5:
+            case 5://join as observer
+                joinGameObserver(sf);
                 break;
-            case 6:
+            case 6:// help
+                System.out.println("1. Logout: end your gaming session" +
+                        "2. List Games: see all available games" +
+                        "3. Create Game: create a new game" +
+                        "4. Join Game: join game as a player" +
+                        "5. Join Observer: watch a game be played");
                 break;
             default:
                 break;
         }
     }
 
+    private void logout(ServerFacade sf)
+    {
+        try
+        {
+            sf.logout(token);
+            System.out.println("You have logged out");
+            token = null;
+        } catch (IOException ex)
+        {
+            System.out.println("Logout failed");
+        }
+    }
+
+    private void listGames(ServerFacade sf)
+    {
+        try
+        {
+            games = sf.listGames(token).games();
+            for (int i = 1; i <= games.size(); i++)
+            {
+
+                System.out.println(i + ". " + games.get(i - 1).gameName());
+                System.out.println("    White: " + games.get(i - 1).whiteUsername());
+                System.out.println("    Black: " + games.get(i - 1).blackUsername());
+            }
+        } catch (IOException ex)
+        {
+            System.out.println("Failed to print games");
+        }
+    }
+
+    private void joinGame(ServerFacade sf)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Game Number:");
+        int gameNum = in.nextInt();
+        int gameID = games.get(gameNum - 1).gameID();
+        System.out.println(games.get(gameNum - 1).gameName());
+        System.out.println("    White: " + games.get(gameNum - 1).whiteUsername());
+        System.out.println("    Black: " + games.get(gameNum - 1).blackUsername());
+
+        String playerColor = null;
+        while (playerColor == null)
+        {
+            System.out.println("\nChoose your Player: \n 1. White\n2. Black");
+            int color = in.nextInt();
+            switch (color)
+            {
+                case 1:
+                    playerColor = "WHITE";
+                    break;
+                case 2:
+                    playerColor = "BLACK";
+                    break;
+                default:
+                    System.out.println("invalid choice");
+                    playerColor = null;
+                    break;
+            }
+        }
+        try
+        {
+            sf.joinGame(new JoinGameRequest(playerColor, gameID), token);
+            System.out.println("Joined Game as " + playerColor);
+            printBoard(playerColor);
+        } catch (IOException ex)
+        {
+            System.out.println("Failed to Join Game");
+        }
+    }
 
 
-    private void printBlack(ChessGame game){
+    private void joinGameObserver(ServerFacade sf)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Game Number:");
+        int gameNum = in.nextInt();
+        int gameID = games.get(gameNum - 1).gameID();
+        try
+        {
+            sf.joinGame(new JoinGameRequest(null, gameID), token);
+            System.out.println("Joined Game as Observer");
+        } catch (IOException ex)
+        {
+            System.out.println("Failed to Join Game");
+        }
+    }
+
+
+    private void printBlack(ChessGame game)
+    {
         PrintBoard printing = new PrintBoard(game);
         printing.printBoardBlack();
     }
 
-    private void printWhite(ChessGame game){
+    private void printWhite(ChessGame game)
+    {
         PrintBoard printing = new PrintBoard(game);
         printing.printBoardWhite();
+    }
+
+    private void printBoard(String playerColor)
+    {
+        if (playerColor == "WHITE")
+        {
+            printWhite(new ChessGame());
+        } else
+        {
+            printBlack(new ChessGame());
+        }
+    }
+
+    private void createGame(ServerFacade sf){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Game Name:");
+        String gameName = in.nextLine();
+        CreateGameRequest game = new CreateGameRequest(gameName);
+        try
+        {
+            sf.createGame(game,token);
+            System.out.println("created Game");
+        }catch(IOException ex){
+            System.out.println("Failed to create Game");
+        }
     }
 }
--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -9,36 +9,46 @@ public class ServerFacade
 {
     private String urlString = "https://localhost:8080";
     private Communicator coms = new Communicator();
-    public LoginResponce register(User newUser)throws IOException
+
+    public LoginResponce register(User newUser) throws IOException
     {
         String token = null;
         String user = new Gson().toJson(newUser);
-        String json = coms.doPost(urlString + "/user",user,token);
-        return new Gson().fromJson(json,LoginResponce.class);
+        String json = coms.doPost(urlString + "/user", user, token);
+        return new Gson().fromJson(json, LoginResponce.class);
 
     }
+
     public LoginResponce login(LoginRequest newLogin) throws IOException
     {
         String token = null;
         String user = new Gson().toJson(newLogin);
-        String json = coms.doPost(urlString + "/session",user,token);
-        return new Gson().fromJson(json,LoginResponce.class);
+        String json = coms.doPost(urlString + "/session", user, token);
+        return new Gson().fromJson(json, LoginResponce.class);
     }
-    public void logout(String token)throws IOException{
-        String json = coms.doDelete(urlString + "/session",token);
+
+    public void logout(String token) throws IOException
+    {
+        String json = coms.doDelete(urlString + "/session", token);
     }
+
     public AllGamesReturn listGames(String token) throws IOException
     {
-        String json = coms.doGet(urlString + "/game",token);
-        return new Gson().fromJson(json,AllGamesReturn.class);
+        String json = coms.doGet(urlString + "/game", token);
+        return new Gson().fromJson(json, AllGamesReturn.class);
     }
-    public void joinGame(String joinReq, String token) throws IOException
+
+    public void joinGame(JoinGameRequest joinReq, String token) throws IOException
     {
-        String json = coms.doPut(urlString + "/game",joinReq,token);
+
+        String json = new Gson().toJson(joinReq);
+        coms.doPut(urlString + "/game", json, token);
     }
-    public GameReturn createGame(String newGame, String token)throws IOException
+
+    public GameReturn createGame(CreateGameRequest newGame, String token) throws IOException
     {
-        String json = coms.doPost(urlString + "/game",newGame,token);
-        return new Gson().fromJson(json,GameReturn.class);
+        String json = new Gson().toJson(newGame);
+        coms.doPost(urlString + "/game", json, token);
+        return new Gson().fromJson(json, GameReturn.class);
     }
 }
